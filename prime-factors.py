90c1215136361873547c18a76b594423fc725c8d
Patrick McElhaney <pmcelhaney@gmail.com>
2009-02-23 10:01:50 -0500

    Finished (almost) prime-factors.py
    I got it working well enough to solve the problem but it returns this:
    
    (3, (4, (5, 2)))
    
    instead of this:
    
    (3, 4, 5, 2)
    
    I'm going to ask Stack Overflow how I can get it to return a flat tuple / list.

--- prime-factors.py
+++ prime-factors.py
@@ -1,12 +1,17 @@
+# Problem 3
 
 # The prime factors of 13195 are 5, 7, 13 and 29.
-
 # What is the largest prime factor of the number 600851475143?
 
 def prime_factors(n):
-	for i in range(2, n+1):
-		if n % i == 0:
-			return i
+	i = 2
+	while i < n:
+	
+	  i = i + 1
+	  
+	  if n % i == 0:
+			return i, prime_factors(n / i)
+	
+	return n
 
-
-print prime_factors(15)
+print prime_factors(120)
